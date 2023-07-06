fc2cb0982e846bc0bfcad99a9628fac0719a6ff7
Hladgerd <hladgerd@gmail.com>
2023-07-06 18:10:06 +0200

    add configuration

---
+++ private/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+const ROOT = 'http://localhost/SeatBooker/public';
+const ASSETS = 'http://localhost/SeatBooker/public/static';
+
+const DB_NAME = 'movie';
+const DB_HOST = 'localhost';
+const DB_USER = 'hladgerd';
+const DB_PASSWORD = 'hladgerd';
+const DB_DRIVER = 'mysql';
