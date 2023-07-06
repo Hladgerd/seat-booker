5a05050fbc063bda1e903a9915e69a0373fe95bf
Hladgerd <hladgerd@gmail.com>
2023-07-07 00:20:37 +0200

    add view method

--- private/Core/Controller.php
+++ private/Core/Controller.php
@@ -1,6 +1,16 @@
 <?php
 
+namespace Core;
+
 class Controller
 {
-
+    public function view($view): void
+    {
+        if(file_exists("../private/Views/" . $view . ".view.php"))
+        {
+            require ("../private/Views/" . $view . ".view.php");
+        }else{
+            echo "Ez az oldal nem található...";
+        }
+    }
 }
