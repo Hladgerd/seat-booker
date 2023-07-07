f1c945883e46a8783f37011d7f24e8d2c8b6b849
Hladgerd <hladgerd@gmail.com>
2023-07-07 03:51:48 +0200

    add sendEmail() method

--- private/Controllers/PaymentController.php
+++ private/Controllers/PaymentController.php
@@ -3,11 +3,37 @@
 namespace Controllers;
 
 use Core\Controller;
+use Core\View;
 
 class PaymentController extends Controller
 {
-    function index(): void
+    public function indexAction(): void
     {
-        $this->view("payment");
+        View::render('seat');
+    }
+
+    public function sendEmail(): void
+    {
+        if (isset($_POST['submit']))
+        {
+            $to = $_POST['email'];
+            $name = $_POST['name'];
+            $from = "Mozi";
+            $message = "Sikeresen kifizette a foglalást";
+            $subject = "Helyfoglalás";
+            $headers = "From:" . $from;
+            $result = mail($to, $subject, $message, $headers);
+
+            if ($result)
+            {
+                echo '<script type="text/javascript">alert("Email elküldve!");</script>';
+
+            }
+            else
+            {
+                echo '<script type="text/javascript">alert("Hoppá! Próbálja meg újra!");</script>';
+            }
+            echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+        }
     }
 }
