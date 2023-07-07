088ae34cc99d08c38b71fc1524232e7d7d958dd3
Hladgerd <hladgerd@gmail.com>
2023-07-07 03:49:34 +0200

    remove redundant path

--- private/config/autoload.php
+++ private/config/autoload.php
@@ -1,9 +1,9 @@
 <?php
 
-require "../Core/App.php";
 require "../Core/Controller.php";
-require "../Core/Database.php";
 require "../Core/Model.php";
+require "../Core/Router.php";
+require "../Core/View.php";
 require "config.php";
 
 
