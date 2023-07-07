4ee4c293955bb39beb85b6fcebae091552dc36c8
Hladgerd <hladgerd@gmail.com>
2023-07-07 03:50:51 +0200

    add db connection

--- private/Core/Model.php
+++ private/Core/Model.php
@@ -1,6 +1,25 @@
 <?php
 
-class Model
+namespace Core;
+
+use PDO;
+use App\Config;
+
+abstract class Model
 {
 
+    protected static function getDB(): mixed
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $dsn = DB_DRIVER . ":host=".DB_HOST.";dbname=".DB_NAME;
+            $db = new PDO($dsn, DB_USER,DB_PASSWORD);
+
+            // Throw an Exception when an error occurs
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return $db;
+    }
 }
