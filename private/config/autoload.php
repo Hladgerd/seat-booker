2a71d748433b5b50f5f9761f0327dca6707ed59f
Hladgerd <hladgerd@gmail.com>
2023-07-09 23:23:37 +0200

    remove Router path

--- private/config/autoload.php
+++ private/config/autoload.php
@@ -1,11 +1,8 @@
 <?php
 
+require "../Core/App.php";
 require "../Core/Controller.php";
 require "../Core/Model.php";
-require "../Core/Router.php";
 require "../Core/View.php";
 require "config.php";
-
-
-
 
