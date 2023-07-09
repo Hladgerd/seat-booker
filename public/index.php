e08bd71cfee8b63fff8a18651a8d9b0bb454cf3c
Hladgerd <hladgerd@gmail.com>
2023-07-09 23:06:48 +0200

    fix route calling

--- public/index.php
+++ public/index.php
@@ -2,11 +2,5 @@
 
 require("../private/config/autoload.php");
 
-$router = new Core\Router();
-
-// Add the routes
-$router->add('', ['controller' => 'SeatController', 'action' => 'indexAction']);
-$router->add('{controller}/{action}');
-
-$router->dispatch($_SERVER['QUERY_STRING']);
+$app = new App();
 
