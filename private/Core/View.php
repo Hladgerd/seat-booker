be0ba81c9a7056d60deb588d78b112cda1b7196d
Hladgerd <hladgerd@gmail.com>
2023-07-10 05:11:00 +0200

    add dataset parameter

--- private/Core/View.php
+++ private/Core/View.php
@@ -4,8 +4,9 @@ namespace Core;
 
 class View
 {
-    public function render(string $view): void
+    public function render(string $view, $data = array()): void
     {
+        extract($data);
         $file = "private/Views/$view.view.php";
 
         if (is_readable($file)) {
