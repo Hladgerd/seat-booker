905e2e3237eeb1a6414c41f8e206559bc7f365e0
Hladgerd <hladgerd@gmail.com>
2023-07-10 05:08:50 +0200

    remove index and redirect methods

--- private/Core/Controller.php
+++ private/Core/Controller.php
@@ -6,23 +6,12 @@ use Models\SeatModel;
 
 abstract class Controller
 {
-    private View $view;
+    protected View $view;
     protected SeatModel $model;
 
     public function __construct()
     {
         $this->view = new View();
         $this->model = new SeatModel();
-    }
-
-    public function index(string $view): void
-    {
-        $this->view->render($view);
-    }
-
-    public function redirect(string $link)
-    {
-        header("Location: ". ROOT . "/".trim($link,"/"));
-        exit();
     }
 }
