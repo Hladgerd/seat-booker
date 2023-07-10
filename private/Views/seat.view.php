b53330c5bb12ffd866d5fee31adb21fcebbefdbf
Hladgerd <hladgerd@gmail.com>
2023-07-10 05:06:41 +0200

    add id, status and price values

--- private/Views/seat.view.php
+++ private/Views/seat.view.php
@@ -1,4 +1,5 @@
 <?php include 'private/Controllers/protect.php'?>
+<?php include 'private/Controllers/Seat.php'?>
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -29,48 +30,22 @@
     <div class="container">
         <div class="screen"></div>
 
-        <?php foreach ($this->oPosts as $oPost): ?>
-            <h1><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>"><?=htmlspecialchars($oPost->title)?></a></h1>
-
-            <p><?=nl2br(htmlspecialchars(mb_strimwidth($oPost->body, 0, 100, '...')))?></p>
-            <p><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Want to see more?</a></p>
-            <p class="left small italic">Posted on <?=$oPost->createdDate?></p>
-
-            <?php require 'inc/control_buttons.php' ?>
-            <hr class="clear" /><br />
-        <?php endforeach ?>
-
         <div class="row">
-            <div class="seat occupied" data-nr="1"></div>
-            <div class="seat occupied" data-nr="2"></div>
-            <div class="seat occupied" data-nr="3"></div>
-            <div class="seat" data-nr="4"></div>
-            <div class="seat" data-nr="5"></div>
-            <div class="seat occupied" data-nr="6"></div>
-            <div class="seat occupied" data-nr="7"></div>
-            <div class="seat occupied" data-nr="8"></div>
+            <?php foreach (array_slice($dataSet,0,7) as $data):?>
+                <div id="<?=$data->seatId?>" class="seat <?=$data->status?>" data-price="<?=$data->price?>"></div>
+            <?php endforeach;?>
         </div>
 
         <div class="row">
-            <div class="seat occupied" data-nr="9"></div>
-            <div class="seat occupied" data-nr="10"></div>
-            <div class="seat occupied" data-nr="11"></div>
-            <div class="seat occupied" data-nr="12"></div>
-            <div class="seat occupied" data-nr="13"></div>
-            <div class="seat occupied" data-nr="14"></div>
-            <div class="seat occupied" data-nr="15"></div>
-            <div class="seat occupied" data-nr="16"></div>
+            <?php foreach (array_slice($dataSet,7,7) as $data):?>
+                <div id="<?=$data->seatId?>" class="seat <?=$data->status?>" data-price="<?=$data->price?>"></div>
+            <?php endforeach;?>
         </div>
 
         <div class="row">
-            <div class="seat occupied" data-nr="17"></div>
-            <div class="seat occupied" data-nr="18"></div>
-            <div class="seat occupied" data-nr="19"></div>
-            <div class="seat occupied" data-nr="20"></div>
-            <div class="seat occupied" data-nr="21"></div>
-            <div class="seat occupied" data-nr="22"></div>
-            <div class="seat occupied" data-nr="23"></div>
-            <div class="seat occupied" data-nr="24"></div>
+            <?php foreach (array_slice($dataSet,14,7) as $data):?>
+                <div id="<?=$data->seatId?>" class="seat <?=$data->status?>" data-price="<?=$data->price?>"></div>
+            <?php endforeach;?>
         </div>
     </div>
 
@@ -78,7 +53,7 @@
         <span id="count">0</span> helyet foglalt le <span id="price">0</span> forintért.
     </p>
     <p>
-        <a href="payment.view.php" id="payment" class="disable_a_href">Fizetek</a>
+        <a href="href="<?=ROOT?>/payment" id="payment" class="disable_a_href">Fizetek</a>
     </p>
 </body>
 
