fbc992ed31d6c3adf11c043c1dce657849fd8dc8
Hladgerd <hladgerd@gmail.com>
2023-07-10 08:18:42 +0200

    refact password

--- private/config/config.php
+++ private/config/config.php
@@ -5,5 +5,5 @@ const ROOT = 'http://localhost/seat_booker/public';
 const DB_NAME = 'movie';
 const DB_HOST = 'localhost';
 const DB_USER = 'hladgerd';
-const DB_PASSWORD = 'hladgerd';
+const DB_PASSWORD = '****';
 const DB_DRIVER = 'mysql';
